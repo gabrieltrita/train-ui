88ad54821ddf3456a2048480b1ec2de994699b4d
Gabriel Cunha <gabriel_cunha@apple.com>
2020-06-17 20:34:00 -0300

    remove methods

--- lib/app/modules/trainings/trainings_page.dart
+++ lib/app/modules/trainings/trainings_page.dart
@@ -21,33 +21,6 @@ class _TrainingPageState extends State<TrainingPage> {
     super.initState();
   }
 
-  Future onDidReceiveLocalNotification(
-      int id, String title, String body, String payload) async {
-    // display a dialog with the notification details, tap ok to go to another page
-    showCupertinoDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-            title: Text(title),
-            content: Text(body),
-            actions: [
-              CupertinoDialogAction(
-                isDefaultAction: true,
-                child: Text('Ok'),
-                onPressed: () async {
-                  Navigator.of(context, rootNavigator: true).pop();
-                  await Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                      builder: (context) => Center(),
-                    ),
-                  );
-                },
-              )
-            ],
-          ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
